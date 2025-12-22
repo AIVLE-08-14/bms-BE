3b1fd991de5a2971f15d6845619f5022e735a5cf
beom710 <beom710@naver.com>
2025-12-22 17:38:12 +0900

    feat: health_check.sh 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,11 @@
+# stop.sh 예시
 #!/bin/bash
-# 실행 중인 backend.jar 프로세스 종료
 PID=$(pgrep -f backend.jar)
-
 if [ -z "$PID" ]; then
-    echo "> 현재 실행 중인 애플리케이션이 없습니다."
+    echo "> 현재 실행 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    exit 0 # 에러 없이 종료
 else
-    echo "> 기존 프로세스 종료: kill -15 $PID"
+    echo "> kill -15 $PID"
     kill -15 $PID
     sleep 5
 fi
